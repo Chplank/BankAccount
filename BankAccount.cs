777cfceba44c54ee314b47e1aa63a0597d267aa2
Chplank <132442564+Chplank@users.noreply.github.com>
2024-11-09 16:49:39 +0100

    Initial Commit

---
+++ BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace BankAccount;
+public class BankAccount
+{
+    private double _amount;
+
+    public BankAccount(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException();
+        }
+        _amount = amount;
+    }
+
+    public string Deposit(double amount)
+    {
+        if (amount < 0)
+        {
+            return "Error: Deposit amount must be positive";
+
+        }
+        if (amount == 0)
+        {
+            return "Error: Deposit amount must be greater than 0";
+        }
+
+        _amount += amount;
+
+        return "Deposit successful";
+    }
+
+    public string Withdraw(double amount)
+    {
+
+        if (amount < 0)
+        {
+            return "Error: Withdrawal amount must be positive";
+
+        }
+        if (amount == 0)
+        {
+            return "Error: Withdrawal amount must be greater than 0";
+        }
+
+        if (_amount - amount < 0)
+        {
+            return "Error: Insufficient funds available";
+        }
+
+        _amount -= amount;
+
+        return "Withdrawal successful";
+    }
+
+    public double Balance()
+    {
+        return Math.Round(_amount, 2);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BankAccount;
+
+internal class Programm
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(1000.00);
+
+        string result1 = account.Deposit(500.00);
+        Console.WriteLine(result1);               // Output: "Deposit successful"
+        Console.WriteLine(account.Balance());       // Output: 1500.00
+
+        string result2 = account.Withdraw(800.00);
+        Console.WriteLine(result2);               // Output: "Withdrawal successful"
+        Console.WriteLine(account.Balance());       // Output: 700.00
+
+        string result3 = account.Withdraw(800.00);
+        Console.WriteLine(result3);               // Output: "Error: Insufficient funds available"
+        Console.WriteLine(account.Balance());       // Output: 700.00
+
+    }
+}
